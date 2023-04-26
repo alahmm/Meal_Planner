bdb7a1b8a2d935aee78cfc4be96867e80b1a4ebb
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-04-26 13:36:42 +0200

    adding special meals to the menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,23 +7,67 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void MealPlanner(String category, String name, List<String> Ingredients) {
-        System.out.printf("%nCategory: %s%n", category);
-        System.out.printf("Name: %s%n", name);
-        System.out.println("Ingredients:");
-        Ingredients.forEach(System.out::println);
-        System.out.println("The meal has been added!");
+    public static String MealPlanner(String category, String name, List<String> Ingredients) {
+        String meal = String.format("%nCategory: %s%nName: %s%nIngredients:%n", category, name);
+        String ingre = "";
+        for (String ingredient : Ingredients
+        ) {
+            ingre += String.format("%s%n", ingredient);
+        }
+        return meal + ingre;
     }
-    public static void main(String[] args) {
-
-        System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        String meal = scanner.next();
-        System.out.println("Input the meal's name:");
-        String nameOfMale = scanner.next();
-        System.out.println("Input the ingredients:");
-        List<String> ingredients = Arrays.stream(scanner.nextLine().trim().split(",")).toList();
-        MealPlanner(meal, nameOfMale, ingredients);
+        String regex = "[a-zA-Z ]+";
+        String Complete_meal = "";
+        while(true) {
+            System.out.print("What would you like to do (add, show, exit)?");
+            scanner.useDelimiter("\\n");
+            String toDo = scanner.next();
+            while (!toDo.equals("add") && !toDo.equals("show") && !toDo.equals("exit")) {
+                break;
+            }
+            switch (toDo) {
+                case "add" -> {
+                    System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+                    String meal = scanner.next();
+                    while (!meal.equals("breakfast") && !meal.equals("lunch") && !meal.equals("dinner")) {
+                        System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+                        meal = scanner.next();
+                    }
+                    System.out.println("Input the meal's name:");
+                    String nameOfMale = scanner.next();
+                    while (!nameOfMale.matches(regex)) {
+                        System.out.println("Wrong format. Use letters only!");
+                        nameOfMale = scanner.next();
+                    }
+                    System.out.println("Input the ingredients:");
+                    List<String> ingredients = Arrays.stream(scanner.next().split(",")).toList();
+                    for (int i = 0; i < ingredients.size(); i++) {
+                        if  (!ingredients.get(i).matches(regex) || ingredients.get(i).equals("") || ingredients.get(i).equals(" ")) {
+                            System.out.println("Wrong format. Use letters only!");
+                            ingredients = Arrays.stream(scanner.next().split(",")).toList();
+                            i = 0;
+                        }
+                    }
+                    System.out.println("The meal has been added!");
+                    Complete_meal += MealPlanner(meal, nameOfMale, ingredients);
+
+                }
+                case "show" ->  {
+                    if (Complete_meal.equals("")) {
+                        System.out.println("No meals saved. Add a meal first.");
+                    } else {
+                        System.out.println(Complete_meal);
+                    }
+                }
+                case "exit" -> {
+                    System.out.println("Bye!");
+                    return;
+                }
+            }
+
+        }
 
     }
 }
