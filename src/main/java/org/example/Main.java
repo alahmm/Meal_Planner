6bca4f423d75efdb6ce77050dd5f48f5295fc96e
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-05-19 11:24:45 +0200

    Making the plan of the whole week and save it in the backed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,6 +25,40 @@ public class Main {
         }
         return meal + ingre;
     }
+    public static String Planner (String day, ResultSet rs, Scanner scanner, String category) throws SQLException {
+        List<String> listOfBreakfast = new ArrayList<>();
+        while (rs.next()) {
+            System.out.println(rs.getString("meal"));
+            listOfBreakfast.add(rs.getString("meal"));
+        }
+        System.out.printf("choose the %s for %s from the list above:%n",category, day);
+        String theChosenBreakfast = scanner.next();
+        while (!listOfBreakfast.contains(theChosenBreakfast)) {
+            System.out.println("This meal doesn't exist. Choose a meal from the list above.");
+            theChosenBreakfast = scanner.next();
+        }
+        return theChosenBreakfast;
+    }
+    public static List<String> PlannerForTheDay(String day, ResultSet rs, Scanner scanner, Statement statement) throws SQLException {
+        List<String> listOfTheDay = new ArrayList<>();
+        System.out.println(day);
+        rs = statement.executeQuery("select meal from meals where category = 'breakfast' order by meals");
+        String WhatToEat = String.format("breakfast: %s", Planner(day, rs, scanner, "breakfast"));
+        listOfTheDay.add(WhatToEat);
+        rs.close();
+        rs = statement.executeQuery("select meal from meals where category = 'lunch'");
+        WhatToEat = String.format("lunch: %s", Planner(day, rs, scanner, "lunch"));
+        listOfTheDay.add(WhatToEat);
+        rs.close();
+        rs = statement.executeQuery("select meal from meals where category = 'dinner'");
+        WhatToEat = String.format("dinner: %s", Planner(day, rs, scanner, "dinner"));
+        listOfTheDay.add(WhatToEat);
+        rs.close();
+        System.out.printf("Yeah! We planned the meals for %s%n", day);
+        System.out.println();
+
+        return listOfTheDay;
+    }
     public static void main(String[] args) throws SQLException {
         Map<Integer, List<String>> map = new HashMap<>();
 
@@ -52,6 +86,13 @@ public class Main {
                 "ingredient varchar(1024)," +
                 "ingredient_id INT," +
                 "meal_id INT" +
+                ")");
+
+        //create table named plan
+        statement.executeUpdate("drop table if exists plan");
+        statement.executeUpdate("create table if not exists plan(" +
+                "plan_text varchar," +
+                "plan_id INT" +
                 ")");
         ResultSet rs = statement.executeQuery("select * from meals");
         while (rs.next()) {
@@ -81,7 +122,7 @@ public class Main {
         String regex = "[a-zA-Z ]+";
 
         while(true) {
-            System.out.print("What would you like to do (add, show, exit)?");
+            System.out.print("What would you like to do (add, show, plan, exit)?");
             scanner.useDelimiter("\\n");
             String toDo = scanner.next();
             while (!toDo.equals("add") && !toDo.equals("show") && !toDo.equals("exit")) {
@@ -161,6 +202,31 @@ public class Main {
                         System.out.printf("Category: %s", whichCategory);
                         System.out.println(Complete_meal);
                     }
+                }
+                case "plan" -> {
+                    Map<String, List<String>> planOfTheWeek = new LinkedHashMap<>();
+                    String[] theDaysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+                    for (String day: theDaysOfTheWeek
+                         ) {
+                        planOfTheWeek.put(day, PlannerForTheDay(day, rs, scanner, statement));
+                    }
+                    String planString = "";
+                    for (Map.Entry<String, List<String>> meal :
+                            planOfTheWeek.entrySet()) {
+                        System.out.println(meal.getKey());
+                        planString += String.format(meal.getKey() + "%n");
+                        for (String variable : meal.getValue()
+                             ) {
+                            System.out.println(variable);
+                            planString += String.format(variable + "%n");
+                        }
+                        System.out.println();
+                        planString += String.format("%n");
+
+                    }
+                    String sql = String.format("insert into plan values ('%s', %d)", planString, 1);
+                    statement.executeUpdate(sql);
+
                 }
                 case "exit" -> {
                     System.out.println("Bye!");
